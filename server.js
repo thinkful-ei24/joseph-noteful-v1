bcb52d319cbce791c349a82150dd8b4582444d45
josephhess <readysteadyco@gmail.com>
2018-09-07 12:47:12 -0700

    get notes by id and search

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const data = require('./db/notes');
 console.log('Hello Noteful!');
 
 const express = require('express');
-
 const app = express();
 
 app.listen(8080, function(){
@@ -15,6 +14,18 @@ app.listen(8080, function(){
   console.error(err);
 });
 
-app.get('/api/notes', (req, res) => {
+app.get('/api/notes', (req, res) => { 
+  const searchTerm = req.query.searchTerm;
+  if (searchTerm) {
+    console.log(searchTerm);
+    return res.json(data.filter(item => item.title.includes(searchTerm)));
+
+  }
   res.json(data);
 });
+
+app.get('/api/notes/:id',(req,res) =>{
+  const note = data.find(note => note.id === Number(req.params.id));
+  res.json(note);
+});
+
