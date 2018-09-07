7217580362a6556ac17192d8de2c3daeb7e80f15
josephhess <readysteadyco@gmail.com>
2018-09-07 10:17:25 -0700

    added first get method

--- server.js
+++ server.js
@@ -5,4 +5,16 @@ const data = require('./db/notes');
 
 console.log('Hello Noteful!');
 
-// INSERT EXPRESS APP CODE HERE...
+const express = require('express');
+
+const app = express();
+
+app.listen(8080, function(){
+  console.info(`Server listening on & ${this.address().port}`);
+}).on('error', err => {
+  console.error(err);
+});
+
+app.get('/api/notes', (req, res) => {
+  res.json(data);
+});
