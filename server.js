ece6431c26af80e429446a3320d69d96674dc8e3
josephhess <readysteadyco@gmail.com>
2018-09-08 12:38:10 -0700

    put updates on notes

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 'use strict';
 
-// Load array of notes
 const data = require('./db/notes');
 const simDB = require('./db/simDB');
 const notes =simDB.initialize(data);
 
-console.log('Hello Noteful!');
-
-const express = require('express');
-const app = express();
 const {PORT} = require('./config');
 const {logger} = require('./middleware/logger');
 
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+app.use(express.json());
 app.use(logger);
 
 // app.use(function (req, res, next) {
@@ -45,7 +45,32 @@ app.get('/api/notes/:id',(req, res, next) =>{
       return next(err);
     }
     res.json(item);
-  })  
+  });  
+});
+
+app.put('/api/notes/:id', (req, res, next) => {
+  const id = req.params.id;
+
+  /***** Never trust users - validate input *****/
+  const updateObj = {};
+  const updateFields = ['title', 'content'];
+
+  updateFields.forEach(field => {
+    if (field in req.body) {
+      updateObj[field] = req.body[field];
+    }
+  });
+
+  notes.update(id, updateObj, (err, item) => {
+    if (err) {
+      return next(err);
+    }
+    if (item) {
+      res.json(item);
+    } else {
+      next();
+    }
+  });
 });
 
 app.listen(PORT, function(){
