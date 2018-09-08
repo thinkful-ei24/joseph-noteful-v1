3623e6b537375bfc2278c9fcf8cbb40fcfb886ab
josephhess <readysteadyco@gmail.com>
2018-09-08 08:51:24 -0700

    error handling middleware

--- server.js
+++ server.js
@@ -12,10 +12,18 @@ const {logger} = require('./middleware/logger.js');
 
 app.use(logger);
 
-app.listen(PORT, function(){
-  console.info(`Server listening on & ${this.address().port}`);
-}).on('error', err => {
-  console.error(err);
+app.use(function (req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  res.status(404).json({ message: 'Not Found' });
+});
+
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    error: err
+  });
 });
 
 app.get('/api/notes', (req, res) => { 
@@ -33,3 +41,8 @@ app.get('/api/notes/:id',(req,res) =>{
   res.json(note);
 });
 
+app.listen(PORT, function(){
+  console.info(`Server listening on & ${this.address().port}`);
+}).on('error', err => {
+  console.error(err);
+});
